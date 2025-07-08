9a0b76e7e8ea1dceaeec9e7fedabaa0db48fe498
Alex Golovin <alex-golovin@yandex.ru>
2025-07-08 22:49:48 +0300

    Lesson 2.1

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,12 +1,18 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var liked = false
+    private var likes = 0
+    private var shares = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,14 +23,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        println(resources.displayMetrics.heightPixels) // 1794
-        println(resources.displayMetrics.widthPixels) // 1080
-        println(resources.displayMetrics.densityDpi) // 420
-        println(resources.displayMetrics.density) // 2.625
+        val likeButton: ImageButton = findViewById(R.id.like_button)
+        val shareButton: ImageButton = findViewById(R.id.share_button)
+        val likesCount: TextView = findViewById(R.id.likes_count)
+        val sharesCount: TextView = findViewById(R.id.shares_count)
 
+        likesCount.text = likes.shortCount()
+        sharesCount.text = shares.shortCount()
+
+        likeButton.setOnClickListener {
+            liked = !liked
+            if (liked) {
+                likeButton.setImageResource(R.drawable.ic_like_filled_24)
+                likes++
+            } else {
+                likeButton.setImageResource(R.drawable.ic_like_outline_24)
+                if (likes > 0) likes--
+            }
+            likesCount.text = likes.shortCount()
+        }
+
+        shareButton.setOnClickListener {
+            shares++
+            sharesCount.text = shares.shortCount()
+        }
     }
 }
 
-
-//        println(R.string.hello) // число
-//        println(getString(R.string.hello)) // "Привет, Мир!" или "Hello World!"
+fun Int.shortCount(): String = when (this) {
+    in 0..999 -> this.toString()
+    in 1_000..9_999 -> "${this / 1000}.${(this % 1000) / 100}K"
+    in 10_000..999_999 -> "${this / 1000}K"
+    in 1_000_000..9_999_999 -> "${this / 1_000_000}.${(this % 1_000_000) / 100_000}M"
+    else -> "${this / 1_000_000}M"
+}
